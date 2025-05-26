cc12e29785b47c2ace227419f23f4b3bfb46e002
Raza <141234596+KMORaza@users.noreply.github.com>
2025-05-27 00:28:55 +0530

    Delete codebase/src/main directory

--- codebase/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module simulation.software.codebase.sweepgenerationsoftware {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens simulation.software.codebase to javafx.fxml;
-    exports simulation.software.codebase;
-}
